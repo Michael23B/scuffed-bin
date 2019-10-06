21c3ec38eb809936a95bab04e9345bfc7b167dc3
yung lean <33080132+Michael23B@users.noreply.github.com>
2019-10-06 12:37:45 +1000

    fix path.join call in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/scuffed-bin'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/scuffed-bin/index.html'));
+res.sendFile(path.join(__dirname,'/dist/scuffed-bin/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
